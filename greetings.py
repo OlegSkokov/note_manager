d053de521b52441ad4f1d77e4ae7065c4880f152
OlegSkokov <o-skokov@mail.ru>
2025-01-13 15:27:55 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,24 @@
+
+
+username = "Иван Иванов"  # Имя пользователя
+title = "Покупка метизов"  # Заголовок заметки
+content = "Купить шурупы, болты, гайки."  # Описание заметки
+status = "Активна"  # Статус заметки
+created_date = "12-01-2025"  # Дата создания заметки
+issue_date = "15-01-2025"  # Дата истечения заметки
+
+# Вывод значений переменных
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
+
+
+
+# git commit -m "Initial commit"
+# [master (root-commit) e1020f3] Initial commit
+# 1 file changed, 1 insertion(+)
+# create mode 100644 README.md
+
